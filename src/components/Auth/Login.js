86974fb9c3875b1c93e8225a1203205bfb4dec25
Daniel Dimitrov <danieldimitrov2304@gmail.com>
2022-12-25 19:43:13 +0200

    Add swapi

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,22 +1,39 @@
+import { useState, useEffect } from "react";
+
 function Login() {
+
+    const [person, setPerson] = useState({});
+
+    useEffect(() => {
+        fetch('https://swapi.dev/api/people/1/')
+            .then(res => res.json())
+            .then(response => {
+                setPerson(response);
+            })
+    }
+        , []);
+
+
+    console.log(person);
+
     return (
         <section id="login-page" className="auth">
-        <form id="login">
-          <div className="container">
-            <div className="brand-logo"></div>
-            <h1>Login</h1>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+            <form id="login">
+                <div className="container">
+                    <div className="brand-logo"></div>
+                    <h1>Login</h1>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
 
-            <label htmlFor="login-pass">Password:</label>
-            <input type="password" id="login-password" name="password" />
-            <input type="submit" className="btn submit" value="Login" />
-            <p className="field">
-              <span>If you don't have profile click <a href="#">here</a></span>
-            </p>
-          </div>
-        </form>
-      </section>
+                    <label htmlFor="login-pass">Password:</label>
+                    <input type="password" id="login-password" name="password" />
+                    <input type="submit" className="btn submit" value="Login" />
+                    <p className="field">
+                        <span>If you don't have profile click <a href="#">here</a></span>
+                    </p>
+                </div>
+            </form>
+        </section>
     )
 }
 
